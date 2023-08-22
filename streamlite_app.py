b67171598474212ab3bce697cdb083f597fb16b4
rogeriotosin <97060863+rogeriotosin@users.noreply.github.com>
2023-08-22 20:16:45 -0300

    Update streamlite_app.py

--- streamlite_app.py
+++ streamlite_app.py
@@ -2,9 +2,12 @@ import streamlit;
 
 streamlit.title("My Parents New Healthy Dinner")
 
-streamlit.header("Breakfast Menu")
+streamlit.header("Breakfast Favorites")
+   
+streamlit.text("🥣 Omega 3 & Blueberry OatMeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
 
-streamlit.text("Omega 3 & Blueberry OatMeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
